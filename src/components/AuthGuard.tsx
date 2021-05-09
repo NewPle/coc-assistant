edf0f30c2f332a688ef3103f4ea41a0223d80034
FugaTakata <maple1213goo@gmail.com>
2021-05-10 01:35:44 +0900

    add: auth guard

---
+++ src/components/AuthGuard.tsx
@@ -0,0 +1,29 @@
+import { ReactNode, VFC } from "react";
+import { Redirect } from "react-router-dom";
+import { useAuth } from "../hooks/auth";
+import routes from "../routes";
+
+interface Props {
+  children: ReactNode;
+}
+
+const AuthGuard: VFC<Props> = ({ children }) => {
+  const { user } = useAuth();
+  const isSignedIn = !!user.uid;
+
+  return <Component {...{ isSignedIn, children }} />;
+};
+
+interface ComponentProps {
+  isSignedIn: boolean;
+  children: ReactNode;
+}
+
+const Component: VFC<ComponentProps> = ({ isSignedIn, children }) => {
+  if (isSignedIn) {
+    return <>{children}</>;
+  }
+  return <Redirect to={routes.root} />;
+};
+
+export default AuthGuard;
