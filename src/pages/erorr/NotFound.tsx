1fd07fd985bfc50fa4594fe67b1ff72eef01f5f4
FugaTakata <maple1213goo@gmail.com>
2021-05-10 11:29:53 +0900

    add: 404 page

---
+++ src/pages/erorr/NotFound.tsx
@@ -0,0 +1,12 @@
+import { IonContent, IonHeader, IonPage, IonTitle } from "@ionic/react";
+import { VFC } from "react";
+
+const NotFound: VFC = () => {
+  return (
+    <IonPage>
+      <IonContent>404 page not found</IonContent>
+    </IonPage>
+  );
+};
+
+export default NotFound;
