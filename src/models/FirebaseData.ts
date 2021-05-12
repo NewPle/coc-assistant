2cb93d6280c62537c3d436fced920b16381b90ba
FugaTakata <maple1213goo@gmail.com>
2021-05-12 23:31:52 +0900

    reafactor: add Data to the end of name

--- src/models/FirebaseData.ts
+++ src/models/FirebaseData.ts
@@ -14,6 +14,6 @@ export interface FirebaseUserSheetsData {
   [key: string]: UserSheet;
 }
 
-export interface FirebaseUserRooms {
+export interface FirebaseUserRoomsData {
   [key: string]: UserRoom;
 }
