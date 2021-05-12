46499e5a0442a3314b9d648430c7a75216d1994b
FugaTakata <maple1213goo@gmail.com>
2021-05-12 14:20:21 +0900

    feat: firebase rtdb routes

---
+++ src/rtdbRoutes.ts
@@ -0,0 +1,65 @@
+const rooms = (roomId?: string, propName?: string) => {
+  const prefix = "/rooms";
+  if (!roomId) {
+    return prefix;
+  }
+  if (roomId.includes("/")) {
+    return null;
+  }
+  if (!propName) {
+    return `${prefix}/${roomId}`;
+  }
+  return `${prefix}/${roomId}/${propName}`;
+};
+
+const sheets = (sheetId?: string) => {
+  const prefix = "/sheets";
+  if (!sheetId) {
+    return prefix;
+  }
+  if (sheetId.includes("/")) {
+    return null;
+  }
+  return `${prefix}/${sheetId}`;
+};
+
+const users = (userId?: string, propName?: string) => {
+  const prefix = "/users";
+  if (!userId) {
+    return prefix;
+  }
+  if (userId.includes("/")) {
+    return null;
+  }
+  if (!propName) {
+    return `${prefix}/${userId}`;
+  }
+  return `${prefix}/${userId}/${propName}`;
+};
+
+const routes = {
+  rooms: {
+    root: rooms(),
+    room: {
+      root: (roomId: string) => rooms(roomId),
+      info: (roomId: string) => rooms(roomId, "info"),
+      messages: (roomId: string) => rooms(roomId, "messages"),
+      sheets: (roomId: string) => rooms(roomId, "sheets"),
+    },
+  },
+  sheets: {
+    root: sheets(),
+    sheet: (sheetId: string) => sheets(sheetId),
+  },
+  users: {
+    root: users(),
+    user: {
+      root: (userId: string) => users(userId),
+      info: (userId: string) => users(userId, "info"),
+      rooms: (userId: string) => users(userId, "rooms"),
+      sheets: (userId: string) => users(userId, "sheets"),
+    },
+  },
+};
+
+export default routes;
