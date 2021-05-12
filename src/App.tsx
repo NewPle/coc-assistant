34852b578a2e6ba4245a682978fb07a72d8bd1ca
FugaTakata <maple1213goo@gmail.com>
2021-05-12 13:21:22 +0900

    feat: add sass and add half modal sass

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ import "@ionic/react/css/display.css";
 /* Theme variables */
 import "./theme/variables.css";
 
-import "./styles/global.css";
+import "./styles/global.scss";
 
 import routes from "./routes";
 import Home from "./pages/Home";
