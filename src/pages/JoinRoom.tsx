e5af547ffb73e7100215a53815528b5fddea3a23
FugaTakata <maple1213goo@gmail.com>
2021-05-12 23:12:12 +0900

    feat: add join room page

--- src/pages/JoinRoom.tsx
+++ src/pages/JoinRoom.tsx
@@ -1,15 +1,130 @@
 import {
   IonBackButton,
+  IonButton,
   IonButtons,
   IonContent,
   IonHeader,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonListHeader,
   IonPage,
   IonTitle,
   IonToolbar,
 } from "@ionic/react";
+import { useCallback, useEffect, useState } from "react";
+import { useHistory } from "react-router";
+import SheetThumnail from "../components/thumbnails/SheetThumbnail";
+import { useAuth } from "../hooks/auth";
+import { useError } from "../hooks/error";
+import { useRoom } from "../hooks/room";
+import { rtdb } from "../lib/firebase";
+import { FirebaseUserSheetsData, RoomInfo, UserSheets } from "../models";
 import { routes } from "../routes";
+import { rtdbRoutes } from "../rtdbRoutes";
 
 const JoinRoom: React.VFC = () => {
+  const history = useHistory();
+  const { user } = useAuth();
+  const { setInfo } = useRoom();
+  const [roomId, setRoomId] = useState<string>("");
+  const [userSheets, setUserSheets] = useState<UserSheets>([]);
+  const [selectedSheetId, setSelectedSheetId] = useState("");
+  const { updateError } = useError();
+
+  const joinRoom = (event: React.FormEvent<HTMLFormElement>) => {
+    try {
+      event.preventDefault();
+      if (!user.uid) {
+        throw new Error("ユーザーが見つかりません");
+      }
+      userSheets.forEach((userSheet) => {
+        if (userSheet.participatingRoomId === roomId) {
+          throw new Error("参加済みのルームです");
+        }
+      });
+      const roomInfoPath = rtdbRoutes.rooms.room.info(roomId);
+      if (!roomInfoPath) {
+        throw new Error();
+      }
+      const roomInfoRef = rtdb.ref(roomInfoPath);
+      roomInfoRef
+        .get()
+        .then((snapshot) => {
+          if (snapshot.exists()) {
+            const roomInfoData: RoomInfo = snapshot.val();
+            if (roomInfoData.masterId === user.uid) {
+              throw new Error("自身がマスターのルームには参加できません");
+            }
+            setInfo(roomInfoData);
+            history.push(routes.room.root);
+          } else {
+            throw new Error();
+          }
+        })
+        .catch((error) => {
+          if (error.message) {
+            updateError(error.message);
+          } else {
+            updateError("内部エラーが発生しました");
+          }
+        });
+    } catch (error) {
+      if (error.message) {
+        updateError(error.message);
+      } else {
+        updateError("内部エラーが発生しました");
+      }
+    }
+  };
+
+  const onSheetClick = useCallback((sheetId: string) => {
+    setSelectedSheetId(sheetId);
+  }, []);
+
+  useEffect(() => {
+    try {
+      if (!user.uid) {
+        throw new Error("ユーザーが見つかりません");
+      }
+
+      const userSheetsPath = rtdbRoutes.users.user.sheets(user.uid);
+      if (!userSheetsPath) {
+        throw new Error();
+      }
+      const userSheetsRef = rtdb.ref(userSheetsPath);
+
+      userSheetsRef
+        .get()
+        .then((snapshot) => {
+          if (snapshot.exists()) {
+            const userSheetsData: FirebaseUserSheetsData = snapshot.val();
+            const userSheets = Object.keys(userSheetsData).map(
+              (key) => userSheetsData[key]
+            );
+            setUserSheets(userSheets);
+          } else {
+            throw new Error("シートが見つかりません");
+          }
+        })
+        .catch((error) => {
+          // todo nested catch
+          if (error.message) {
+            updateError(error.message);
+          } else {
+            updateError("内部エラーが発生しました");
+          }
+        });
+    } catch (error) {
+      if (error.message) {
+        updateError(error.message);
+      } else {
+        updateError("内部エラーが発生しました");
+      }
+    }
+  }, []);
+
   return (
     <IonPage>
       <IonHeader>
@@ -26,6 +141,33 @@ const JoinRoom: React.VFC = () => {
             <IonTitle size="large">JoinRoom</IonTitle>
           </IonToolbar>
         </IonHeader>
+        <form onSubmit={joinRoom}>
+          <IonItem>
+            <IonLabel position="floating">ルームID</IonLabel>
+            <IonInput
+              name="roomName"
+              value={roomId}
+              // todo use String may be bad way
+              onIonChange={(event) => setRoomId(String(event.detail.value))}
+            />
+          </IonItem>
+          <IonList>
+            <IonListHeader>キャラクターを選択</IonListHeader>
+            {userSheets.map((userSheet) => {
+              return (
+                <SheetThumnail
+                  key={userSheet.sheetId}
+                  sheet={userSheet}
+                  onSheetClick={onSheetClick}
+                  selected={userSheet.sheetId === selectedSheetId}
+                />
+              );
+            })}
+          </IonList>
+          <IonButton type="submit" disabled={!selectedSheetId || !roomId}>
+            参加
+          </IonButton>
+        </form>
       </IonContent>
     </IonPage>
   );
