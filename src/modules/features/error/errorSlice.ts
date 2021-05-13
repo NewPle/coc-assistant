f10f1adcd714d89159c59ffe492d5a9db7cc42d6
FugaTakata <maple1213goo@gmail.com>
2021-05-13 18:44:59 +0900

    feat: add where

--- src/modules/features/error/errorSlice.ts
+++ src/modules/features/error/errorSlice.ts
@@ -2,19 +2,24 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 export interface ErrorState {
   message: string | null;
+  where: string | null;
 }
 
 const initialState: ErrorState = {
   message: null,
+  where: null,
 };
 
 export const errorSlice = createSlice({
   name: "error",
   initialState,
   reducers: {
-    updateError: (state, action: PayloadAction<string>) => {
+    updateError: (state, action: PayloadAction<ErrorState>) => {
       // todo it may caouse performance problem
-      return Object.assign({}, state, { message: action.payload });
+      return Object.assign({}, state, {
+        message: action.payload.message,
+        where: action.payload.where,
+      });
     },
   },
 });
