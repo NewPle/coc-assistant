5d303a998ec67fec9c4899a65b6424c65c04ff59
FugaTakata <maple1213goo@gmail.com>
2021-05-14 16:30:11 +0900

    feat: add history.push('/') when signed in

--- src/hooks/auth.ts
+++ src/hooks/auth.ts
@@ -8,33 +8,53 @@ import { useAppDispatch, useAppSelector } from "./redux";
 import firebase from "firebase/app";
 import { useHistory } from "react-router";
 import { routes } from "../routes";
+import { useError } from "./error";
+import { firebaseError } from "../values/firebaseError";
 
 export const useAuth = () => {
   const history = useHistory();
   const dispatch = useAppDispatch();
   const user = useAppSelector(({ user }) => user);
+  const { updateError } = useError();
 
   const signUp = (email: string, password: string, displayName: string) => {
-    return auth.setPersistence(firebase.auth.Auth.Persistence.LOCAL).then(() =>
-      auth
-        .createUserWithEmailAndPassword(email, password)
-        .then((firebaseUser) => {
-          if (firebaseUser.user) {
-            firebaseUser.user.updateProfile({ displayName });
-          }
-        })
-    );
+    return auth
+      .setPersistence(firebase.auth.Auth.Persistence.LOCAL)
+      .then(() =>
+        auth
+          .createUserWithEmailAndPassword(email, password)
+          .then((firebaseUser) => {
+            if (firebaseUser.user) {
+              firebaseUser.user.updateProfile({ displayName });
+            }
+          })
+      )
+      .then(() => history.push(routes.root))
+      .catch((error) => {
+        console.error(error);
+        updateError(firebaseError(error, "signup"));
+        return false;
+      });
   };
 
   const signIn = (email: string, password: string) => {
     return auth
       .setPersistence(firebase.auth.Auth.Persistence.LOCAL)
-      .then(() => auth.signInWithEmailAndPassword(email, password));
+      .then(() => auth.signInWithEmailAndPassword(email, password))
+      .then(() => history.push(routes.root))
+      .catch((error) => {
+        console.error(error);
+        updateError(firebaseError(error, "signin"));
+      });
   };
 
   const signOut = () => {
     history.push(routes.signin);
-    return auth.signOut();
+    return auth.signOut().catch((error) => {
+      console.error(error);
+      updateError(firebaseError(error, "signout"));
+      return false;
+    });
   };
 
   useEffect(() => {
