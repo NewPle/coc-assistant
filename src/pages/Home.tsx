dd327720594124199f393356fab6f1c44fe6a854
FugaTakata <maple1213goo@gmail.com>
2021-05-16 03:27:19 +0900

    make home looks good

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,6 +8,7 @@ import {
 } from "@ionic/react";
 import { useAuth } from "../hooks/auth";
 import { routes } from "../routes";
+import Logo from "../images/icon.png";
 
 const Home: React.VFC = () => {
   const { signOut } = useAuth();
@@ -19,21 +20,48 @@ const Home: React.VFC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        <IonButton expand="full" routerLink={routes.sheetList}>
-          シート一覧
-        </IonButton>
-        <IonButton expand="full" routerLink={routes.createRoom}>
-          ルーム作成
-        </IonButton>
-        <IonButton expand="full" routerLink={routes.joinRoom}>
-          ルームに参加
-        </IonButton>
-        <IonButton expand="full" routerLink={routes.roomList}>
-          参加中ルーム一覧
-        </IonButton>
-        <IonButton expand="full" color="danger" onClick={() => signOut()}>
-          サインアウト
-        </IonButton>
+        <div style={{ backgroundColor: "black", height: "100%" }}>
+          <img src={Logo} alt="そこはかとない宇宙の深淵" />
+          <IonButton
+            expand="block"
+            className="ion-padding-horizontal"
+            routerLink={routes.sheetList}
+          >
+            シート一覧
+          </IonButton>
+          <IonButton
+            expand="block"
+            className="ion-padding-horizontal"
+            routerLink={routes.createRoom}
+          >
+            ルーム作成
+          </IonButton>
+          <IonButton
+            expand="block"
+            className="ion-padding-horizontal"
+            routerLink={routes.joinRoom}
+          >
+            ルームに参加
+          </IonButton>
+          <IonButton
+            expand="block"
+            className="ion-padding-horizontal"
+            routerLink={routes.roomList}
+          >
+            参加中ルーム一覧
+          </IonButton>
+          <div className="ion-padding" />
+          <div className="ion-padding" />
+          <div className="ion-padding" />
+          <IonButton
+            expand="block"
+            className="ion-padding-horizontal"
+            color="danger"
+            onClick={() => signOut()}
+          >
+            サインアウト
+          </IonButton>
+        </div>
       </IonContent>
     </IonPage>
   );
