a0495a77b5aebadcb6d3e36e936a3b1fa9d87075
FugaTakata <maple1213goo@gmail.com>
2021-05-16 03:36:18 +0900

    add background color

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -19,51 +19,48 @@ const Home: React.VFC = () => {
           <IonTitle>ホーム</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent>
-        <div style={{ backgroundColor: "black", height: "100%" }}>
-          <img src={Logo} alt="そこはかとない宇宙の深淵" />
-          <IonButton
-            expand="block"
-            className="ion-padding-horizontal"
-            routerLink={routes.sheetList}
-          >
-            シート一覧
-          </IonButton>
-          <div className="ion-padding-top" />
-          <IonButton
-            expand="block"
-            className="ion-padding-horizontal"
-            routerLink={routes.createRoom}
-          >
-            ルーム作成
-          </IonButton>
-          <div className="ion-padding-top" />
-          <IonButton
-            expand="block"
-            className="ion-padding-horizontal"
-            routerLink={routes.joinRoom}
-          >
-            ルームに参加
-          </IonButton>
-          <div className="ion-padding-top" />
-          <IonButton
-            expand="block"
-            className="ion-padding-horizontal"
-            routerLink={routes.roomList}
-          >
-            参加中ルーム一覧
-          </IonButton>
-          <div className="ion-padding" />
-          <div className="ion-padding" />
-          <IonButton
-            expand="block"
-            className="ion-padding-horizontal"
-            color="danger"
-            onClick={() => signOut()}
-          >
-            サインアウト
-          </IonButton>
-        </div>
+      <IonContent style={{ "--ion-background-color": "#000" }}>
+        <img src={Logo} alt="そこはかとない宇宙の深淵" />
+        <IonButton
+          expand="block"
+          className="ion-padding-horizontal"
+          routerLink={routes.sheetList}
+        >
+          シート一覧
+        </IonButton>
+        <div className="ion-padding-top" />
+        <IonButton
+          expand="block"
+          className="ion-padding-horizontal"
+          routerLink={routes.createRoom}
+        >
+          ルーム作成
+        </IonButton>
+        <div className="ion-padding-top" />
+        <IonButton
+          expand="block"
+          className="ion-padding-horizontal"
+          routerLink={routes.joinRoom}
+        >
+          ルームに参加
+        </IonButton>
+        <div className="ion-padding-top" />
+        <IonButton
+          expand="block"
+          className="ion-padding-horizontal"
+          routerLink={routes.roomList}
+        >
+          参加中ルーム一覧
+        </IonButton>
+        <div className="ion-padding" />
+        <IonButton
+          expand="block"
+          className="ion-padding-horizontal"
+          color="danger"
+          onClick={() => signOut()}
+        >
+          サインアウト
+        </IonButton>
       </IonContent>
     </IonPage>
   );
